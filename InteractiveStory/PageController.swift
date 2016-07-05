6baf1b938466a28dbae82a828417c9f2e556ffb5
Tosypitarz <Tosypitarz@gmail.com>
2016-07-05 00:30:17 -0500

    constraints fixed

---
+++ InteractiveStory/PageController.swift
@@ -0,0 +1,73 @@
+//
+//  PageController.swift
+//  InteractiveStory
+//
+//  Created by Tosin on 2016-07-04.
+//  Copyright © 2016 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+    
+    let artwork = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .System)
+    let secondChoiceButton = UIButton(type: .System)
+    
+    var page: Page?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        //view.backgroundColor = .blueColor()
+        if let page = page
+        {
+            //print(page.story.text)
+            artwork.image = page.story.artwork
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewWillLayoutSubviews() {
+        view.addSubview(artwork)
+        artwork.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activateConstraints([
+            artwork.topAnchor.constraintEqualToAnchor(view.topAnchor),
+            artwork.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor),
+            artwork.rightAnchor.constraintEqualToAnchor(view.rightAnchor),
+            artwork.leftAnchor.constraintEqualToAnchor(view.leftAnchor)])
+        
+        
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
